d1e945511470bc13e3d01e3ce2b424fa21b12e2b
Luuk van Oijen <lazyluuk.channel@gmail.com>
2022-07-18 00:22:10 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,29 +3,30 @@
 #[derive(Default)]
 pub struct FrameInfo<'frame> {
     buf: &'frame mut [u8],
-    resolution: (usize, usize),
 }
 
 impl FrameInfo<'_> {
     pub fn new(mem_addr: u32) -> Self {
         Self {
             buf: unsafe { std::slice::from_raw_parts_mut(mem_addr as *mut u8, 168*72*4) },
-            resolution: (168, 72),
         }
     }
 }
 
 #[no_mangle]
-pub extern "C" fn draw_unsafe(mem_addr: u32) {
+pub extern "C" fn tick(mem_addr: u32, input: u64, delta_s: f32) {
     draw(FrameInfo::new(mem_addr))
 }
 
 fn draw(info: FrameInfo) {
-    for x in 0..info.resolution.0 {
-        for y in 0..info.resolution.1 {
-            let i = (x + y * info.resolution.0) * 4;
-            info.buf[i  ] = 255;
-            info.buf[i+1] = 0;
+    for x in 0..168 {
+        for y in 0..72 {
+            let uvx = x as f32 / 168f32;
+            let uvy = y as f32 / 72f32;
+
+            let i = (x + y * 168) * 4;
+            info.buf[i  ] = (uvx * 255.0) as u8;
+            info.buf[i+1] = (uvy * 255.0) as u8;
             info.buf[i+2] = 0;
             info.buf[i+3] = 255;
         }
